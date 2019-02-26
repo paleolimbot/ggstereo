33def22411ee0dc7dd4feab27cd20a9165bb2c42
Dewey Dunnington <dewey@fishandwhistle.net>
2019-02-25 21:09:00 -0400

    Update source for the deep hollow dataset

--- R/data.R
+++ R/data.R
@@ -4,7 +4,11 @@
 #' A sample dataset of strike and dip measurements (in degrees) for bedding and
 #' cleavage planes in the Deep Hollow area, Kings County, Nova Scotia.
 #'
-#' @source
-#' \url{https://novascotia.ca/natr/meb/data/pubs/10re01/10re01_18White.pdf}
+#' @source White, C.E. (2010). Pre-Carboniferous Bedrock Geology of the
+#' Annapolis Valley Area (NTS 21A/14, 15 and 16; 21H/01 and 02), Southern Nova
+#' Scotia. In Mineral Resources Branch, Report of Activities 2009; Nova Scotia
+#' Department of Natural Resources, Report ME 2010-1, p. 137-155.
+#' \url{https://novascotia.ca/natr/meb/data/pubs/10re01/10re01_18White.pdf} in
+#' \url{https://novascotia.ca/natr/meb/pdf/10re01.asp}
 #'
 "deep_hollow"
